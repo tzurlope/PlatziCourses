756a9f18f50b9aa5ea145eb6dcf84d5e7565f15a
Tzuriel López <tzuriel.lm@hotmail.com>
2023-02-02 18:19:44 -0500

    Clase de Valores en JS

---
+++ JS-Básico/valores.js
@@ -0,0 +1,29 @@
+// VALORES PRIMITIVOS
+
+// Esto es un valor tipo numero, solo es necesario escribir lo números
+
+40
+
+// Para guardar textos es hacerlo entre comillas ""
+
+"Tzuriel López Montaño"
+
+// Los booleanos se definen solo con la palabra true or false
+
+true
+false
+
+// Hay un tipo de valores llamados vacíos o empty, se usan para guardar partes de código donde se espera tener un valor en el futuro
+
+null
+undefined
+
+// VALORES NO PRIMITIVOS O TIPO OBJETOS
+
+// Array, se ponen entre llaves y separados por comas
+
+[1,2,3]
+
+// Objeto, se ponen entre corchetes, el nombre del objeto, dos puntos y su valor
+
+{ nombre: "Tzuriel"}
