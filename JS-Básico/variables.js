6a5d6ab384b837c8c0fea4bd0cb593d8da1b5e40
Tzuriel López <tzuriel.lm@hotmail.com>
2023-02-02 19:57:09 -0500

    Clase de Variables

---
+++ JS-Básico/variables.js
@@ -0,0 +1,29 @@
+// Para las variables se usa la palabra "var" que está reservada por JS, se usa "var NOMBRE_DE_LA_VARIABLE = VALOR_DE_LA_VARIABLE"
+
+var nombre = "Tzuriel"
+
+// Estados de una variable
+
+    // Declarar, se declara el nombre de una variable, sin asignarle un valor
+
+    var edad;
+
+    // Inicializar, se le da un valor después de declararla
+
+    edad = 30;
+
+    edad
+
+
+// Ejemplo de Variable con Array
+
+var elementos = ["Computador","Celular"];
+
+// Ejemplo de variable con Objeto
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona
