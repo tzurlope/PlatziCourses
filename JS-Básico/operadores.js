68ec729afb3bf93d259dab035333f1067b96bb0b
Tzuriel López <tzuriel.lm@hotmail.com>
2023-02-08 15:54:28 -0500

    Clase de Operadores en JS

---
+++ JS-Básico/operadores.js
@@ -0,0 +1,65 @@
+// OPERADORES
+
+// Los tipos de operadores en el lenguaje son: aritméticos, asignación y comparación
+
+// BINARIOS, pueden ser usados con numeros y strings
+
+3 + 2
+50 - 10
+10 * 20
+20 / 2
+"Tzuriel " + "López Montaño"
+
+// UNITY OPERATORS, El caracyer ! se usa como negación, en el ejemplo de abajo, el resultado sería true
+
+!false
+
+// ASIGNACIÓN, se usa = para asignar valores
+
+var a = 1;
+
+// COMPARACIÓN, se usa == para comparar y saber si dos valores pueden significar los mismo, como en el caso de un string y un numero del mismo valor, y se usa === cuando se quiere comparar y que sean estrictamente iguales, del mismo tipo y valor.
+
+3 == "3"
+true
+
+3 === "3"
+false
+
+5 < 3
+false
+
+5 > 3
+true
+
+5 <= 6
+true
+
+6 >= 6
+false
+
+// VALIDACIÓN, se usa && como una condición AND para que ambas condiciones se cumplan y se usa || como una condición OR en la que solo una de las condiciones se debe cumplir
+
+a && b
+
+a || b
+
+// INCREMENTO, se usa ++ para incrementar el valor de una variable en 1, se usa += y el número, para incrementar el número de una variable según lo deseado
+
+var edad = 40
+
+edad++
+41
+
+edad+= 5
+45
+
+// DECREMENTO, se usa -- para decrementar el valor de una variable en 1, se usa += y el número, para decrementar el número de una variable según lo deseado
+
+var edad = 40
+
+edad--
+41
+
+edad-= 5
+45
