ae8ee27b6761cd52df56e9547afcece8a7a3d66a
Tzuriel López <tzuriel.lm@hotmail.com>
2023-02-27 19:38:09 -0500

    Leyendo HTML desde JS

--- JS-Básico/Manipulación del DOM/script.js
+++ JS-Básico/Manipulación del DOM/script.js
@@ -1,1 +1,20 @@
 console.log('Hello, World')
+
+
+// Para seleccionar como variable elementos del HTML se usa la funcon querySelector yentre corchetes y comillas el nombre del label, class o id y se usa igual que en CSS
+
+const h1 = document.querySelector('h1')
+const p = document.querySelector('p')
+const parrafito = document.querySelector('.parrafito')
+const pid = document.querySelector('#pid')
+const input = document.querySelector('input')
+
+console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input
+})
